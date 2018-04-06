9af86ee150b375df5df40a6de31e4e0759ce5417
Ilya <ilyail3@gmail.com>
2018-04-06 17:25:10 -0400

    Refactor the code

--- digest.go
+++ digest.go
@@ -14,12 +14,24 @@ import (
 	"crypto/x509"
 	"log"
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"bytes"
+	"io"
+	"crypto"
+	"os"
+	"encoding/csv"
 )
 
 const FilenameRegex = "\\/([0-9]{12})_CloudTrail-Digest_([a-z0-9-]+)_" +
 	"([a-zA-Z0-9-]+)_([a-z0-9-]+)_([0-9]{8}T[0-9]{6}Z)\\.json\\.gz$"
 
 const FilenameDateFormat = "20060102T150405Z"
+
+const FileOpenMode = os.O_RDWR | os.O_TRUNC | os.O_CREATE
+
+const FilePermission = 0600
 
 type DigestCompare struct {
 	Objects *treeset.Set
@@ -28,6 +40,36 @@ type DigestCompare struct {
 	svc *s3.S3
 	cred *credentials.Credentials
 	publicKeys map[string]*rsa.PublicKey
+}
+
+type Validate struct {
+	Bucket       string
+	Key          string
+	ExpectedSig  string
+	ExpectedHash string
+}
+
+type LogFile struct {
+	S3Bucket        string `json:"s3Bucket"`
+	S3Object        string `json:"s3Object"`
+	HashValue       string `json:"hashValue"`
+	HashAlgorithm   string `json:"hashAlgorithm"`
+	NewestEventTime string `json:"newestEventTime"`
+	OldestEventTime string `json:"oldestEventTime"`
+}
+
+type DigestFile struct {
+	PreviousDigestS3Bucket      string  `json:"previousDigestS3Bucket"`
+	PreviousDigestS3Object      string  `json:"previousDigestS3Object"`
+	DigestSignatureAlgorithm    string  `json:"digestSignatureAlgorithm"`
+	PreviousDigestSignature     string  `json:"previousDigestSignature"`
+	PreviousDigestHashAlgorithm string  `json:"previousDigestHashAlgorithm"`
+	PreviousDigestHashValue     string  `json:"previousDigestHashValue"`
+	DigestPublicKeyFingerprint  string  `json:"digestPublicKeyFingerprint"`
+	DigestEndTime               string  `json:"digestEndTime"`
+	DigestS3Bucket              string  `json:"digestS3Bucket"`
+	DigestS3Object              string  `json:"digestS3Object"`
+	LogFiles                    []LogFile `json:"logFiles"`
 }
 
 func NewDigestCompare(svc *s3.S3, cred *credentials.Credentials) *DigestCompare {
@@ -138,8 +180,159 @@ func (result *DigestCompare)GetPublicKeys() error {
 		}
 	}
 
-	fmt.Println("Keys")
-	fmt.Println(result.publicKeys)
+	return nil
+}
+
+func streamToByte(stream io.Reader) []byte {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(stream)
+	return buf.Bytes()
+}
+
+func(result *DigestCompare) verifySignature(keyFingerPrint string, hashed [32]byte, signature string) error {
+	signed, err := hex.DecodeString(signature)
+
+	if err != nil {
+		return fmt.Errorf("hex decode:%v", err)
+	}
+
+	pubicKey, exists := result.publicKeys[keyFingerPrint]
+
+	if !exists {
+		return fmt.Errorf("no key for fingerprint:%s", keyFingerPrint)
+	}
+
+	return rsa.VerifyPKCS1v15(
+		pubicKey,
+		crypto.SHA256,
+		hashed[:],
+		signed)
+}
+
+func(result *DigestCompare) ValidateObjects() error {
+	fileWriter, err := os.OpenFile(
+		"/tmp/files.csv",
+		FileOpenMode,
+		FilePermission)
+
+	if err != nil {
+		return err
+	}
+
+	defer fileWriter.Close()
+
+	writer := csv.NewWriter(fileWriter)
+	defer writer.Flush()
+
+	writer.Write([]string{
+		"S3Bucket",
+		"S3Object",
+		"HashValue",
+		"HashAlgorithm",
+		"NewestEventTime",
+		"OldestEventTime",
+	})
+
+	for !result.Objects.Empty() {
+		it := result.Objects.Iterator()
+		it.End()
+		it.Prev()
+
+		lastKey := it.Value().(string)
+		parts := strings.SplitN(lastKey, "/", 2)
+
+		v := Validate{
+			Bucket: parts[0],
+			Key: parts[1],
+			ExpectedSig: "",
+			ExpectedHash: "",
+		}
+
+		for result.Objects.Contains(v.Bucket + "/" + v.Key) {
+			f, err := result.svc.GetObject(&s3.GetObjectInput{
+				Bucket: &v.Bucket,
+				Key:    &v.Key})
+
+			if err != nil {
+				data := err.(s3.RequestFailure)
+				// Objects can be deleted by AWS while the process is running.
+				// in such case, just ignore the record, this doesn't break the chain(since those files
+				// are truncated by age, therefor oldest-last links of the chain will be impacted first)
+				if data.Code() == "NoSuchKey" {
+					result.Objects.Remove(v.Bucket + "/" + v.Key)
+					break
+				} else {
+					return fmt.Errorf(
+						"error getting key bucket:%s key:%s error:%v",
+						v.Bucket,
+						v.Key,
+						err)
+				}
+			}
+
+			data := streamToByte(f.Body)
+			f.Body.Close()
+
+			decoder := json.NewDecoder(bytes.NewReader(data))
+
+			file := &DigestFile{}
+			decoder.Decode(file)
+
+			hashed := sha256.Sum256(data)
+			calculatedHash := hex.EncodeToString(hashed[:])
+
+			if v.ExpectedHash != "" && calculatedHash != v.ExpectedHash {
+				return fmt.Errorf(
+					"bad hash encountered on key:%s, expected:%s, got:%s",
+					v.Key,
+					calculatedHash,
+					v.ExpectedHash)
+			}
+			if *f.Metadata["Signature-Algorithm"] != "SHA256withRSA" {
+				return fmt.Errorf("unkown signature algorithem encountered:%s", *f.Metadata["Signature-Algorithm"])
+			}
+
+			dataToSign := fmt.Sprintf("%s\n%s/%s\n%s\n%s",
+				file.DigestEndTime,
+				file.DigestS3Bucket,
+				file.DigestS3Object,
+				hex.EncodeToString(hashed[:]),
+				file.PreviousDigestSignature)
+
+			signatureHash := sha256.Sum256([]byte(dataToSign))
+
+			err = result.verifySignature(file.DigestPublicKeyFingerprint, signatureHash, *f.Metadata["Signature"])
+
+			if err != nil {
+				return fmt.Errorf(
+					"failed to validate bucket:%s key:%s signature:%v",
+					v.Bucket,
+					v.Key,
+					err)
+			}
+
+			result.Objects.Remove(v.Bucket + "/" + v.Key)
+
+			fmt.Printf("Verified object %s/%s\n", v.Bucket, v.Key)
+
+			v.Bucket = file.PreviousDigestS3Bucket
+			v.Key = file.PreviousDigestS3Object
+			v.ExpectedSig = file.PreviousDigestSignature
+			v.ExpectedHash = file.PreviousDigestHashValue
+
+			for _, file := range file.LogFiles {
+				writer.Write([]string{
+					file.S3Bucket,
+					file.S3Object,
+					file.HashValue,
+					file.HashAlgorithm,
+					file.NewestEventTime,
+					file.OldestEventTime,
+				})
+			}
+		}
+
+	}
 
 	return nil
 }
